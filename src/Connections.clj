f0b0ada75c103e653c942b0e05dc954a3f97fdde
Urs Reupke <ur@idos.de>
2011-10-06 15:58:11 +0200

    More readable code at the doorstep

--- src/Connections.clj
+++ src/Connections.clj
@@ -26,25 +26,25 @@
     (or (= (connection1 query1) (connection2 query2))))
 
 (defn connected
-    ([person1 person2 relation query1 query2]
+    ([person1 relation person2 query1 query2]
         (let [connectedPersons (list person1 person2)]
             (if (not (list-contains? connectedPersons query1 query2))
                 "None"
-                (if (= relation "Friendly")
+                (if (= relation "likes")
                     "Cooperative"
-                    (if (= relation "Antagonistic")
+                    (if (= relation "hates")
                         "Uncooperative")))))
     ([connection1 connection2 query1 query2]
         (if (not (are-linked? connection1 connection2 query1 query2)) "None"
             (if (common-attitude-towards-link? connection1 connection2 query1 query2) "Cooperative" "Uncooperative"))))
 
-(defn- connect [name1 name2 relation]
+(defn- connect [name1 relation name2]
     (fn
         ([query] (if (= query name1) name2 (if (= query name2) name1)))
-        ([query1 query2] (connected name1 name2 relation query1 query2))))
+        ([query1 query2] (connected name1 relation name2 query1 query2))))
 
 (defn befriend [name1 name2]
-    (connect name1 name2 "Friendly"))
+    (connect name1 "likes" name2))
 
 (defn oppose [name1 name2]
-    (connect name1 name2 "Antagonistic"))
+    (connect name1 "hates" name2))
