6cf750ab98c3465c006f931ba41f46c94c741dab
Urs Reupke <ursreupke@gmail.com>
2011-10-10 22:18:12 +0200

    Cond absolves us of some ifs.

--- src/People.clj
+++ src/People.clj
@@ -18,15 +18,13 @@
   ([person other-person]
     (is-linked-to? person other-person []))
   ([person other-person already-inspected-persons]
-    (if (list-contains? already-inspected-persons person)
-      false
-      (if (knows? person other-person)
-        true
-        (do (if (nil? person)
-              false
-              (do (def contacts-of-person (get relationships person))
-                (def direct-link (first contacts-of-person))
-                (is-linked-to? direct-link other-person (conj already-inspected-persons person)))))))))
+    (cond
+      (nil? person) false
+      (knows? person other-person) true
+      (list-contains? already-inspected-persons person) false
+      :else (do (def contacts-of-person (get relationships person))
+              (def direct-link (first contacts-of-person))
+              (is-linked-to? direct-link other-person (conj already-inspected-persons person))))))
 
 (def introduce
   (fn ([person other-person]
