e33a8b04102bb3d9e06db40c8aa1c17008593f8d
Urs Reupke <ursreupke@gmail.com>
2011-10-10 17:18:15 +0200

    Two-sided relationships

--- src/People.clj
+++ src/People.clj
@@ -7,7 +7,14 @@
 (defn knows? [person other-person]
   (= other-person (get relationships person)))
 
-(defn introduce [person other-person]
-  (def relationships (assoc relationships person other-person)))
+(defn know-each-other? [person other-person]
+  (and (knows? person other-person) (knows? other-person person)))
+
+(def introduce
+  (fn ([person other-person]
+        (def relationships (assoc relationships person other-person)))
+    ([person other-person & more]
+      (introduce person other-person)
+      (apply introduce person more))))
 
 (defn person [name] {:name name})
--- test/People_Test.clj
+++ test/People_Test.clj
@@ -4,13 +4,23 @@
 
 (def Urs (person "Urs"))
 (def Sandra (person "Sandra"))
+(def Georg (person "Georg"))
 
 (deftest peopleKnowPeopleTheyAreIntroducedTo
-  (def Urs (introduce Urs Sandra))
+  (introduce Urs Sandra)
   (is (true? (knows? Urs Sandra))))
 
 (deftest introductionsAreUnidirectional
-  (def Urs (introduce Urs Sandra))
+  (introduce Urs Sandra)
   (is (false? (knows? Sandra Urs))))
+
+(deftest introductionsCanInvolveManyPeople
+  (introduce Urs Sandra Georg)
+  (is (true? (knows? Urs Georg))))
+
+(deftest mutualIntroductions
+  (introduce Urs Sandra)
+  (introduce Sandra Urs)
+  (is (true? (know-each-other? Urs Sandra))))
 
 (run-tests)
