bb84147eb0d159eb49ba472aaa19397ddd0b1776
Urs Reupke <ursreupke@gmail.com>
2011-10-11 21:44:39 +0200

    People are immutable

--- src/People.clj
+++ src/People.clj
@@ -2,37 +2,40 @@
   (:use Collections)
   (:use clojure.contrib.def))
 
-(def relationships {})
+(defn knows-someone-like?
+  ([someone other-person]
+    (let [names (seq (:relationships someone))]
+      (some #(= (:name other-person) (:name %)) names))))
 
-(defn forget-everyone [] (def relationships {}))
-
-(defn knows? [person other-person]
-  (if (= person other-person)
+(defn knows? [someone other-person]
+  (if (= someone other-person)
     true
-    (not (nil? (list-contains? (get relationships person) other-person)))))
+    (not (nil? (knows-someone-like? someone other-person)))))
 
-(defn know-each-other? [person other-person]
-  (and (knows? person other-person) (knows? other-person person)))
+(defn know-each-other? [someone other-person]
+  (and (knows? someone other-person) (knows? other-person someone)))
 
 (defn is-linked-to?
-  ([person other-person]
-    (is-linked-to? person other-person []))
-  ([person other-person already-inspected-persons]
+  ([someone other-person]
+    (is-linked-to? someone other-person []))
+  ([someone other-person already-inspected-persons]
     (cond
-      (nil? person) false
-      (knows? person other-person) true
-      (list-contains? already-inspected-persons person) false
-      :else (do (def contacts-of-person (get relationships person))
+      (nil? someone) false
+      (knows? someone other-person) true
+      (list-contains? already-inspected-persons someone) false
+      :else (do (def contacts-of-person (:relationships someone))
               (def direct-link (first contacts-of-person))
-              (is-linked-to? direct-link other-person (conj already-inspected-persons person))))))
+              (is-linked-to? direct-link other-person (conj already-inspected-persons someone))))))
+
+(def person)
 
 (def introduce
-  (fn ([person other-person]
-        (def contacts [])
-        (def contacts (conj (get relationships person) other-person))
-        (def relationships (assoc relationships person contacts)))
+  (fn ([someone other-person]
+        (person (:name someone) (conj (:relationships someone) other-person)))
     ([person other-person & more]
       (introduce person other-person)
       (apply introduce person more))))
 
-(defn person [name] {:name name})
+(defn person
+  ([name] (person name []))
+  ([name contacts] {:name name, :relationships contacts}))
--- test/People_Test.clj
+++ test/People_Test.clj
@@ -3,8 +3,6 @@
   (:use People))
 
 
-(defn forget [test] (forget-everyone) (test))
-
 (defn populate [test]
   (def Urs (person "Urs"))
   (def Sandra (person "Sandra"))
@@ -16,60 +14,60 @@
   (is (true? (knows? Urs Urs))))
 
 (deftest peopleKnowPeopleTheyAreIntroducedTo
-  (introduce Urs Sandra)
+  (def Urs (introduce Urs Sandra))
   (is (true? (knows? Urs Sandra))))
 
 (deftest introductionsMatter
   (is (false? (knows? Urs Sandra))))
 
 (deftest introductionsAreUnidirectional
-  (introduce Urs Sandra)
+  (def Urs (introduce Urs Sandra))
   (is (false? (knows? Sandra Urs))))
 
 (deftest introductionsCanInvolveSomePeople
-  (introduce Urs Sandra Georg)
+  (def Urs (introduce Urs Sandra Georg))
   (is (true? (knows? Urs Georg))))
 
 (deftest peopleRememberEvenAfterMoreIntroductions
-  (introduce Urs Sandra)
-  (introduce Urs Georg)
+  (def Urs (introduce Urs Sandra))
+  (def Urs (introduce Urs Georg))
   (is (true? (knows? Urs Sandra))))
 
 (deftest mutualIntroductionsAreMutuallyRemembered
-  (introduce Urs Sandra)
-  (introduce Sandra Urs)
+  (def Urs (introduce Urs Sandra))
+  (def Sandra (introduce Sandra Urs))
   (is (true? (know-each-other? Urs Sandra))))
 
 (deftest youAreLinkedByIntroductions
-  (introduce Urs Sandra)
+  (def Urs (introduce Urs Sandra))
   (is (true? (is-linked-to? Urs Sandra))))
 
 (deftest peopleYouKnowLinkYouToOtherPeople
-  (introduce Urs Sandra)
-  (introduce Sandra Georg)
+  (def Sandra (introduce Sandra Georg))
+  (def Urs (introduce Urs Sandra))
   (is (true? (is-linked-to? Urs Georg))))
 
 (deftest someDeadEndsDontHinderALink
-  (introduce Urs Sandra)
-  (introduce Sandra Heiner)
-  (introduce Sandra Georg)
+  (def Sandra (introduce Sandra Heiner))
+  (def Sandra (introduce Sandra Georg))
+  (def Urs (introduce Urs Sandra))
   (is (true? (is-linked-to? Urs Georg))))
 
 (deftest linksGoALongWay
-  (introduce Urs Sandra)
-  (introduce Sandra Heiner)
-  (introduce Heiner Georg)
+  (def Heiner (introduce Heiner Georg))
+  (def Sandra (introduce Sandra Heiner))
+  (def Urs (introduce Urs Sandra))
   (is (true? (is-linked-to? Urs Georg))))
 
 (deftest somePeopleCannotBeLinked
-  (introduce Urs Sandra)
+  (def Urs (introduce Urs Sandra))
   (is (false? (is-linked-to? Urs Georg))))
 
 (deftest recognizesEvenCircularLinks
-  (introduce Urs Sandra)
-  (introduce Sandra Georg)
-  (introduce Georg Urs)
+  (def Urs (introduce Urs Sandra))
+  (def Sandra (introduce Sandra Georg))
+  (def Georg (introduce Georg Urs))
   (is (false? (is-linked-to? Urs Heiner))))
 
-(use-fixtures :each forget populate)
+(use-fixtures :each populate)
 (run-tests)
